3aa040812821b1024f362097300ffe5379d91a25
steve-24-Jh <stevenjohnson2408@gmail.com>
2025-12-08 04:18:17 +0000

    Lab 6 changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="dt w-100 border-box pa3 ph5-ns">
-      <a className="dtc v-mid mid-gray link dim w-25" href="/" title="Home">
-        <img src="https://img.logoipsum.com/280.svg" className="dib w2 h2 br-100" alt="Site Name" />
-      </a>
+
+      <Link className="dtc v-mid mid-gray link dim w-25" to="/" title="Home">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" 
+          className="dib w2 h2 br-100"   // 🔥 size reverted to original
+          alt="Site Logo"
+        />
+      </Link>
+
       <div className="dtc v-mid w-75 tr">
-        <a className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="/" title="Products">Products</a>
-        <a className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" href="/cart" title="Cart">Cart</a>
-        <a className="link dim dark-gray f6 f5-ns dib" href="/contact" title="/contact">Contact</a>
+        <Link className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to="/">Products</Link>
+        <Link className="link dim dark-gray f6 f5-ns dib mr3 mr4-ns" to="/cart">Cart</Link>
+        <Link className="link dim dark-gray f6 f5-ns dib" to="/contact">Contact</Link>
       </div>
     </nav>
-
   );
 }
 
